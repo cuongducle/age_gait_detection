cf5c187a28f68ca07efa6196499296f0b7d2e349
cuongld-vbee <cuongld@vbee.vn>
2023-01-12 11:46:27 +0700

    first commit

---
+++ trainer.py
@@ -0,0 +1,40 @@
+
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from dataset import AutomaticExtractionDataset, read_data
+from module import AgeGaitModule,AgeGaitModel
+import torch.utils.data as data
+from pytorch_lightning.loggers import TensorBoardLogger
+
+logger = TensorBoardLogger("tb_logs", name="my_model")
+
+
+path = "./AutomaticExtractionData_IMUZCenter"
+label_file = "IDGenderAgelist.csv"
+# datasest = AutomaticExtractionDataset(path, label_file)
+train_set, valid_set = read_data(path, label_file)
+    
+print("train size", len(train_set),"valid size", len(valid_set))
+train_loader = DataLoader(train_set,batch_size=8,shuffle=True)
+valid_loader = DataLoader(valid_set,batch_size=8,shuffle=True)
+
+# model
+module = AgeGaitModule(AgeGaitModel())
+
+# train model
+trainer = pl.Trainer(logger=logger,max_epochs=50)
+trainer.fit(model=module, train_dataloaders=train_loader,val_dataloaders=valid_loader)
+
+# save model
+trainer.save_checkpoint("model.ckpt")
+
+# autoencoder = LitAutoEncoder(Encoder(), Decoder())
+# optimizer = autoencoder.configure_optimizers()
+
+# for batch_idx, batch in enumerate(train_loader):
+#     loss = autoencoder.training_step(batch, batch_idx)
+
+#     loss.backward()
+#     optimizer.step()
+#     optimizer.zero_grad()
